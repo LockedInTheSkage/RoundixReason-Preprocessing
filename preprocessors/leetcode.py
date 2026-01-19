fbc9ee961479ad0e22018e056a503f60fe86674e
LockedInTheSkage <skareistad@gmail.com>
2026-01-19 15:18:21 +0100

    Supress warnings

--- preprocessors/leetcode.py
+++ preprocessors/leetcode.py
@@ -1,4 +1,7 @@
+import os
 import signal
+import threading
+import warnings
 
 
 class CodeExecutionTimeout(Exception):
@@ -9,27 +12,44 @@ def timeout_handler(signum, frame):
     raise CodeExecutionTimeout("Test execution exceeded timeout")
 
 
+def silent_excepthook(args):
+    pass
+
+
 def validate_code_solution(solution_code, test_code, entry_point, prompt_helpers, timeout_seconds=2):
+    old_excepthook = threading.excepthook
+    threading.excepthook = silent_excepthook
+
+    devnull = os.open(os.devnull, os.O_WRONLY)
+    old_stderr_fd = os.dup(2)
+    old_stdout_fd = os.dup(1)
+    os.dup2(devnull, 2)
+    os.dup2(devnull, 1)
+    os.close(devnull)
+
     try:
-        signal.signal(signal.SIGALRM, timeout_handler)
-        signal.alarm(timeout_seconds)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
 
-        namespace = {}
-        exec(prompt_helpers, namespace)
-        exec(solution_code, namespace)
+            signal.signal(signal.SIGALRM, timeout_handler)
+            signal.alarm(timeout_seconds)
 
-        if '().' in entry_point:
-            class_name, method_name = entry_point.split('().')
-            obj = namespace[class_name]()
-            entry_point_func = getattr(obj, method_name)
-        else:
-            entry_point_func = namespace[entry_point]
+            namespace = {}
+            exec(prompt_helpers, namespace)
+            exec(solution_code, namespace)
 
-        exec(test_code, namespace)
-        namespace['check'](entry_point_func)
+            if '().' in entry_point:
+                class_name, method_name = entry_point.split('().')
+                obj = namespace[class_name]()
+                entry_point_func = getattr(obj, method_name)
+            else:
+                entry_point_func = namespace[entry_point]
 
-        signal.alarm(0)
-        return True
+            exec(test_code, namespace)
+            namespace['check'](entry_point_func)
+
+            signal.alarm(0)
+            return True
 
     except CodeExecutionTimeout:
         signal.alarm(0)
@@ -38,6 +58,13 @@ def validate_code_solution(solution_code, test_code, entry_point, prompt_helpers
     except Exception:
         signal.alarm(0)
         return False
+
+    finally:
+        os.dup2(old_stderr_fd, 2)
+        os.dup2(old_stdout_fd, 1)
+        os.close(old_stderr_fd)
+        os.close(old_stdout_fd)
+        threading.excepthook = old_excepthook
 
 
 def preprocess_leetcode(example):
